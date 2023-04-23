4bfdec50dd4cc145ae21fcd32699687ef3b4a9c8
Dan Hsu <hsu86110@gmail.com>
2023-04-23 12:23:22 +0800

    feature: add approve on Join game

--- Frontend/src/hooks/useContract.ts
+++ Frontend/src/hooks/useContract.ts
@@ -1,11 +1,13 @@
 import React, { useMemo } from "react";
-import { useCall, useEthers, ThunderCoreTestnet } from "@usedapp/core";
+import { useEthers, ThunderCoreTestnet } from "@usedapp/core";
 import TreasuryHuntABI from "@/abis/TreasureHunt.json";
+import ERC20MockABI from "@/abis/ERC20Mock.json";
 import { utils } from "ethers";
 import { Contract, ContractInterface } from "@ethersproject/contracts";
 import ADDRESSES from "@/constants/addresses";
 import { TypedContract } from "@usedapp/core/dist/esm/src/model";
 import { TreasureHunt } from "@/abis/types/TreasureHunt";
+import { ERC20Mock } from "@/abis/types/ERC20Mock";
 
 export function useContract<T extends Contract>(
   address: string,
@@ -22,5 +24,14 @@ export const useTreasuryHuntContract = () => {
   return useContract<TreasureHunt>(
     address.TREASURY_HUNT,
     new utils.Interface(TreasuryHuntABI.abi)
+  );
+};
+
+export const useMockUSDTContract = () => {
+  const { chainId } = useEthers();
+  const address = ADDRESSES[chainId ?? ThunderCoreTestnet.chainId];
+  return useContract<ERC20Mock>(
+    address.MOCK_USDT_TOKEN,
+    new utils.Interface(ERC20MockABI.abi)
   );
 };
